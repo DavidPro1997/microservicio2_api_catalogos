e15b5b40880c1e042efd669b371d0d728885231f
DavidPro1997 <david_pro1997@outlook.com>
2024-12-09 17:53:53 -0500

    cd

--- app/models.py
+++ app/models.py
@@ -78,7 +78,8 @@ class CatalogosBase:
                     "ninos": resultado[5],
                     "dias": resultado[6],  # Ajusta el índice según la estructura de tu tabla
                     "noches": resultado[7],
-                    "descripcion": resultado[8],  
+                    "descripcion": resultado[8],
+                    "rutaPDF": resultado[9],  
                     "estrellas": resultado[10],   # Ajusta el índice según la estructura de tu tabla
                     "destino": resultado[12],
                     "imagenURL": resultado[13]
